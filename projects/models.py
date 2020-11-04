d5deac021696818847e8cfabefcec40e2f9aea1e
richardkefa <richardkefa@gmail.com>
2020-11-04 21:47:59 +0300

    backup

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Projects(models.Model):
+  project_name = models.CharField(max_length=50)
+  description = models.TextField()
+  live_link = models.CharField()
+  
+  
+  def __str__(self):
+    return self.project_name
+  
+  def save_project(self):
+    self.save()
+    
+  def get_projects(cls):
+    projects = cls.objects.all()
+    return projects
+    
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,redirect
-from
-
+from django.http import HttpResponse,Http404
+from .models import Projects
 # Create your views here.
+def welcome(request):
+  projects = Projects.get_projects()
+  return render(request,"all-projects/projects", {"projects":projects})
